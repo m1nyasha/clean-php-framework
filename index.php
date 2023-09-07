b5fd60b31e87fe51666cdfba3e87c1adfbac76a8
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-07 21:27:08 +0300

    Добавил класс App

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\App;
+
 define('APP_PATH', __DIR__);
 
 require_once APP_PATH.'/vendor/autoload.php';
 
-$routes = require APP_PATH.'/config/routes.php';
+$app = new App();
 
-$uri = $_SERVER['REQUEST_URI'];
-
-$routes[$uri]();
+$app->run();
