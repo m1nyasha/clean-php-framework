68e095a82accfd19e0ad3500dc70fe1a94076e7c
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-07 21:44:58 +0300

    Инициализация маршрутов

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -4,13 +4,35 @@ namespace App\Router;
 
 class Router
 {
-    public function dispatch(string $uri): void
+    private array $routes = [
+        'GET' => [],
+        'POST' => [],
+    ];
+
+    public function __construct()
+    {
+        $this->initRoutes();
+    }
+
+    public function dispatch(string $uri, string $method): void
     {
         $routes = $this->routes();
 
         $routes[$uri]();
     }
 
+    private function initRoutes(): void
+    {
+        $routes = $this->routes();
+
+        foreach ($routes as $route) {
+            $this->routes[$route->getMethod()][$route->getUri()] = $route;
+        }
+    }
+
+    /**
+     * @return Route[]
+     */
     private function routes(): array
     {
         return require APP_PATH.'/config/routes.php';
