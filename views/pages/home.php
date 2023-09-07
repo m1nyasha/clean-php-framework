c9ce38127a62ca5733c18c2288eca00e66169944
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-07 22:31:16 +0300

    PHPDoc для параметров шаблона

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * @var \App\Kernel\View\View $view
+ */
+?>
+
 <?php $view->component('start') ?>
 <h1>Home page</h1>
 <?php $view->component('end') ?>
--- views/pages/movies.php
+++ views/pages/movies.php
@@ -1,3 +1,9 @@
+<?php
+/**
+ * @var \App\Kernel\View\View $view
+ */
+?>
+
 <?php $view->component('start') ?>
 <h1>Movies page</h1>
 <?php $view->component('end') ?>
