393db8626eb13f98b1160d7b90f54d81079aedc7
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-08 11:08:07 +0300

    Применение методов check() и user()

--- views/components/header.php
+++ views/components/header.php
@@ -1,4 +1,12 @@
-<header>
-    <p>User: email</p>
-    <button>Logout</button>
-</header>
+<?php
+/**
+ * @var \App\Kernel\Auth\AuthInterface $auth
+ */
+?>
+
+<?php if ($auth->check()) { ?>
+    <header>
+        <p>User: <?php echo $auth->user()['email'] ?></p>
+        <button>Logout</button>
+    </header>
+<?php } ?>
