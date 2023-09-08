0ea715c019bf3dcfa8e83d97ff9550eb2827913e
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-08 11:19:13 +0300

    Добавил класс Middleware

---
+++ kernel/Middleware/Middleware.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Kernel\Middleware;
+
+class Middleware implements MiddlewareInterface
+{
+    public function check(array $middlewares)
+    {
+        // TODO: Implement check() method.
+    }
+}
