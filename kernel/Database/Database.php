274b71756edecb9e832a44b3c9e9e519d23c04a1
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-08 10:03:24 +0300

    Применение конфига в классе Database

--- kernel/Database/Database.php
+++ kernel/Database/Database.php
@@ -2,17 +2,33 @@
 
 namespace App\Kernel\Database;
 
+use App\Kernel\Config\ConfigInterface;
+
 class Database implements DatabaseInterface
 {
     private \PDO $pdo;
 
-    public function __construct()
-    {
-        $this->pdo = new \PDO(
-            'mysql:host=localhost;dbname=php-mvc',
-            'root',
-            'root'
-        );
+    public function __construct(
+        private ConfigInterface $config
+    ) {
+        $driver = $this->config->get('database.driver');
+        $host = $this->config->get('database.host');
+        $port = $this->config->get('database.port');
+        $database = $this->config->get('database.database');
+        $username = $this->config->get('database.username');
+        $password = $this->config->get('database.password');
+        $charset = $this->config->get('database.charset');
+
+        try {
+            $this->pdo = new \PDO(
+                "$driver:host=$host;port=$port;dbname=$database;charset=$charset",
+                $username,
+                $password
+            );
+        } catch (\PDOException $exception) {
+            echo "Connection failed: {$exception->getMessage()}";
+            exit;
+        }
     }
 
     public function insert(string $table, array $data): int
