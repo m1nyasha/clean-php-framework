d7530a10b3723bcd749f02aa6ed06c0ae6c64d90
Mikhail Protasevich <m1nyasha@yandex.ru>
2023-09-08 10:47:36 +0300

    Добавление и реализация метода first()

--- kernel/Database/DatabaseInterface.php
+++ kernel/Database/DatabaseInterface.php
@@ -7,4 +7,6 @@ interface DatabaseInterface
     public function connect(): void;
 
     public function insert(string $table, array $data): int;
+
+    public function first(string $table, array $conditions = []): ?array;
 }
